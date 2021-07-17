1b080562d05bb6d695d28b646ff3b1a35fbf4b96
Intiqam <test123456>
2021-07-17 09:55:53 +0400

    Initial commit

---
+++ src/main/java/com/ingress/ms6task/Ms6TaskApplication.java
@@ -0,0 +1,13 @@
+package com.ingress.ms6task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms6TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms6TaskApplication.class, args);
+	}
+
+}
